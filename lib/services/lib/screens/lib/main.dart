7fb99b3b1388e85fd52d6052165e20ba2c94b25b
baransengul21 <baransengul213443@gmail.com>
2026-08-06 14:20:51 +0300

    Update main.dart

--- lib/services/lib/screens/lib/main.dart
+++ lib/services/lib/screens/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
 
 void main() {
   runApp(const HemenKuryeApp());
@@ -18,7 +18,8 @@ class HemenKuryeApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFFF8FAFC),
         fontFamily: 'system-ui',
       ),
-      home: const HomeScreen(),
+      // Uygulama artık doğrudan Giriş Ekranı ile başlıyor
+      home: const LoginScreen(),
     );
   }
 }
